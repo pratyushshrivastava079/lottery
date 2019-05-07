546bfb6c458b3efc93d7d6aa7fa360a849e3375c
pratyushtech4i <=>
2019-05-07 17:37:23 +0530

    viewport added on all files

--- 2d-betform.php
+++ 2d-betform.php
@@ -40,6 +40,8 @@ $sql = "SELECT * FROM `users` WHERE `id` = '$id'";
 <head>
 
 	<title>Logout | Lottery System</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
--- users.php
+++ users.php
@@ -54,6 +54,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 <head>
 
 	<title>Logout | Lottery System</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
