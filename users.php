01d18da3674636c0ec969b37573016e00cc058e7
pratyushtech4i <=>
2019-05-08 17:15:24 +0530

    navbar shifted down

--- users.php
+++ users.php
@@ -96,6 +96,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
 	  crossorigin="anonymous"></script>
 
+	 <style type="text/css">
+	 	
+	 	.navbar-nav{
+
+	 		width: 100%!important;
+	 		/*text-align: center;*/
+	 	}
+
+	 	.caps{
+
+	 		display: inline-block!important;
+	 		width: 90%;
+	 		text-align: center;
+	 	}
+
+	 </style>
+
 </head>
 
 <body>
@@ -108,7 +125,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     			
 	    		<div class="navbar-header">
 	      				
-	    			<a class="navbar-brand" href="login.php">Lottery System</a>
+	    			<a class="navbar-brand" href="login.php">Home</a>
 	    			
 	    			 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                         <span class="icon-bar"></span>
@@ -118,40 +135,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 				
 	    		</div>
 					
-				<div class="collapse navbar-collapse" id="myNavbar">
+				<div class="caps navbar-brand" id="myNavbar">
 
-		    		<ul class="nav navbar-nav navbar-right">
-		      				
-		    			<li class="active"><a href="login.php">Home</a></li>
-		      			
-		      			<?php if($_SESSION['userlevel'] == "A1"){?>
-
-		    			<li><a href="users.php">Users</a></li>
-
-		    			<li><a href="add-users.php">Add Users</a></li>
-
-		    			<?php }?>
-		      				
-
-		    			<li><a href="2d-betform.php">2D</a></li>
-		    			
-		    			<li><a href="3d-betform.php">3D</a></li>
-		    			
-		    			<li><a href="#">Results</a></li>
-		    			
-		    			<li><a href="#">Reports</a></li>
-
-		      			<?php if(isset($_SESSION['userid'])){?>
-		      				
-			      			<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-		      			
-		      			<?php }else{?>
-
-			      			<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-
-		      			<?php }?>
-	    			
-		    		</ul>
+					<span><?php $today = date("M / d / Y h:i:s A"); echo $today; ?></span>
 
 		    	</div>
 		  	
@@ -246,6 +232,43 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 			</div>
 
 			<?php }?>						           
+
+				<div class="row text-centre">
+
+					<ul class="nav navbar-nav">
+		      				
+		    			<li class="active"><a href="login.php">Home</a></li>
+		      			
+		      			<?php if($_SESSION['userlevel'] == "A1"){?>
+
+		    			<li><a href="users.php">Users</a></li>
+
+		    			<li><a href="add-users.php">Add Users</a></li>
+
+		    			<?php }?>
+		      				
+
+		    			<li><a href="2d-betform.php">2D</a></li>
+		    			
+		    			<li><a href="3d-betform.php">3D</a></li>
+		    			
+		    			<li><a href="#">Results</a></li>
+		    			
+		    			<li><a href="#">Reports</a></li>
+
+		      			<?php if(isset($_SESSION['userid'])){?>
+		      				
+			      			<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+		      			
+		      			<?php }else{?>
+
+			      			<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+
+		      			<?php }?>
+	    			
+		    		</ul>
+
+		    	</div>
 				 
 				<div class="table-responsive">
 				 
